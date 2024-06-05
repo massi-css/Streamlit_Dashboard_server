78de83b46fcaf6d3de9fc8826b0e713b41b6e1c8
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2024-06-05 11:29:14 +0100

    fixed some bugs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -15,12 +15,75 @@ export const sendEmail = async (email, subject, text) => {
             pass: process.env.EMAIL_PASS,
         },
     });
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Email Template</title>
+          <style>
+              body {
+                  color: #E1DAD3;
+                  font-family: Arial, sans-serif;
+                  margin: 0;
+                  padding: 0;
+                  width: 90% !important;
+                  overflow-x: hidden;
+              }
+              .container {
+                  border-radius: 10px;
+                  background-color: #000000 !important;
+                  color: #ffffff;
+                  font-family: Arial, sans-serif;
+                  width: 90%;
+                  padding: 20px;
+                  text-align: center;
+                  overflow-x: hidden;
+              }
+              .image {
+                  margin: 0 auto;
+                  width: 340px;
+                  height: auto;
+                  display: block;
+              }
+              .text {
+                  text-align: center;
+                  margin-top: 10px;
+                  background-color: #000000 !important;
+                  border-radius: 10px;
+                  font-weight: bold;
+                  font-size: 28px;
+                  padding: 10px;
+                  color: #E1DAD3;
+                  font-family: Arial, sans-serif;
+              }
+              h1{
+                  color: #E1DAD3;
+                  font: 700 40px/1.5 'Open Sans', sans-serif;
+                  text-align: center;
+                  padding-top: 20px;
+                  padding-bottom: 20px;
+                  margin-left: 10px;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <h1>Alert !</h1>
+              <img src="https://i.imgur.com/7VAlgLG.png" alt="Email Image" class="image">
+              <p class="text">${text}</p>
+          </div>
+      </body>
+      </html>
+    `;
+
     await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject,
-        text,
-    })
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject,
+      html: htmlContent,
+    });
     console.log("Email sent successfully");
     return true;
   } catch (error) {
